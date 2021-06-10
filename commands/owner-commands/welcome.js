8955585dcfc5c8511d51d0603dc69668dfce0409
LCypherus <km1609@gmail.com>
2021-06-10 15:28:18 +0200

    Fixed the welcome message command

--- commands/owner-commands/welcome.js
+++ commands/owner-commands/welcome.js
@@ -12,7 +12,7 @@ module.exports = {
 
     permissions: ['ADMINISTRATOR'],
     
-    callback: async ({ message, text }) => {
+    callback: async ({ message, text, client }) => {
         const cache = {}
         
         const guildId = message.guild.id 
@@ -29,5 +29,39 @@ module.exports = {
         }, {
             upsert: true
         })
+
+        client.on("guildMemberAdd", async (member) => {
+            const { guild } = member
+    
+            let data = cache[guild.id]
+    
+            if (!data) {
+                console.log('FETCHING FROM DATABASE')
+    
+                const result = await welcomeSchema.findOne({ _id: guild.id })
+    
+                cache[guild.id] = data = [result.channelId, result.text]
+            }
+    
+            const channelId = data[0]
+            const text = data[1]
+    
+            const channel = guild.channels.cache.get(channelId)
+            channel.send(text.replace(/<@>/g, `<@${member.id}>`))
+        })
     }
+}
+
+module.exports.config = {
+    // The display name that server owners will see.
+    // This can be changed at any time.
+    displayName: 'Welcome Message',
+    
+    // The name the database will use to set if it is enabled or not.
+    // This should NEVER be changed once set, and users cannot see it.
+    dbName: 'WELCOME MESSAGE',
+    
+    // Being true means a database connection must be present before the
+    // feature is enabled.
+    loadDBFirst: true
 }
