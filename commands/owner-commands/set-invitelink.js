8c87f0d0913668b3afb44c697ee509a827ade239
LCypherus <km1609@gmail.com>
2021-06-10 15:28:03 +0200

    Added a setinvite and invitelink command

---
+++ commands/owner-commands/set-invitelink.js
@@ -0,0 +1,33 @@
+const commandInvitelinkSchema = require('@schemas/command-invitelink-schema')
+
+module.exports = {
+    name: 'set-invitelink',
+    commands: ['setinvitelink', 'setinvite'],
+    category: 'Owner Commands',
+    description: 'Sets the invite link',
+    minArgs: 1,
+    maxArgs: 1,
+    expectedArgs: '<invitelink>',
+    permissions: ['ADMINISTRATOR'],
+
+    callback: async ({message, text}) => {
+        const guildId = message.guild.id
+        const invitelinkend = text
+
+        await commandInvitelinkSchema.findOneAndUpdate(
+            {
+                _id: guildId,
+            },
+            {
+                _id: guildId,
+                invitelink: invitelinkend,
+            },
+            {
+                upsert: true,
+            }
+        )
+
+        message.channel.send(`The invitelink for this bot is now ${invitelinkend}`)
+        console.log(invitelinkend)
+    },
+}
---
+++ commands/player-commands/get-invitelink.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const commandInvitelinkSchema = require('@schemas/command-invitelink-schema')
+
+module.exports = {
+    name: 'get-invitelink',
+    commands: ['invitelink'],
+    
+    category: 'Player Commands',
+    description: 'Receive this server\'s invite link.',
+
+    minArgs: 0,
+    maxArgs: 0,
+    
+    callback: async ({ message }) => {
+        const { guild } = message
+        const result = await commandInvitelinkSchema.findOne({ _id: guild.id })
+        const invitelink = result.invitelink
+
+        message.channel.send(`https://discord.gg/${invitelink}`)
+
+        const inviteLinkEmbed = new Discord.MessageEmbed()
+            .setTitle('Pen and Players Roleplayer Server Invite Link')
+            .setURL(`https://discord.gg/${invitelink}`)
+            .setDescription('This is the invite link to invite players to this server.')
+            .setThumbnail('https://cdn.discordapp.com/attachments/834882298268221460/840171923093585940/icon.png')
+            .addFields(
+              { name: 'Link', value: `https://discord.gg/${invitelink}` },
+            )
+	          .setFooter('&invitelink - Contact the server owners when you\'re having problems with the invite link.');
+
+        message.channel.send(inviteLinkEmbed);
+    }
+}
