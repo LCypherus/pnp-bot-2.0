4d4c20b0e28d0d89fda55b539d757804522bb27c
LCypherus <km1609@gmail.com>
2021-06-10 15:27:13 +0200

    Added &clearchannel

--- commands/channel-admin-commands/clear-channel.js
+++ commands/channel-admin-commands/clear-channel.js
@@ -1,16 +1,15 @@
 module.exports = {
-    // Best practice for the built-in help menu
     name: 'clear-channel',
     commands: ['clearchannel', 'cc'],
-    description: 'Clears a channel from all messages.',
+    description: 'Clears a channel from the number of messages written in the argument.',
     category: 'Channel Admin Commands',
-    minArgs: 0,
-    maxArgs: 0,
+    maxArgs: 1,
+    expectedArgs: '<Number of messages to delete>',
     
     // Invoked when the command is actually ran
-    callback: ({ message }) => {
-        message.channel.messages.fetch().then((results) => {
-            message.channel.bulkDelete(results)
-        })
+    callback: ({ message, args }) => {
+        const number = parseInt(args)
+
+        message.channel.bulkDelete(number)
     }
 }
